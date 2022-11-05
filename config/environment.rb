d2610721f3d90504584ebd9f4ee7800410952b9b
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-05 15:25:09 +0300

    code to run in development environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+#This is an environment variable. it determines whether our code is running in a development  environment or a test environment
 ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
