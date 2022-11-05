9a94ccc8994a62ffa13218b6be63a406b5be89b9
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-05 15:43:47 +0300

    added columns to our artist table

--- db/migrate/20221105122549_create_artists.rb
+++ db/migrate/20221105122549_create_artists.rb
@@ -1,4 +1,13 @@
 class CreateArtists < ActiveRecord::Migration[6.1]
   def change
+    #always remember table names are  plural 
+    # T is a special Active Record migration object that helps add different columns to the table
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre 
+      t.integer :age
+      t.string :hometown
+      # the id column is gemerated automatically for every table! no need to specify it here.
+    end
   end
 end
