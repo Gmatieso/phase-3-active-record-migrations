2af9ca28c6aba7200dad4be97d7139bfefcf5053
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-05 16:44:12 +0300

    Add favorite flower to artist

--- db/migrate/20221105131723_add_favorite_flower_to_artists.rb
+++ db/migrate/20221105131723_add_favorite_flower_to_artists.rb
@@ -1,6 +1,6 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration[6.1]
+class AddFavoriteFlowerToArtists < ActiveRecord::Migration[6.1]
   #Told Active Record to add a column to the artists table called favorite_food and it should contain a string 
   def change
-    add_column :artists, :favorite_food, :string 
+    add_column :artists, :favorite_flower, :string 
   end
 end
